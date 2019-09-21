41a6e531b1b4860483c9371bc6b924d47d49868e
mohamedsaad <mahameds19999@gmail.com>
2019-09-21 11:35:48 +0200

    edit the session code

--- code/src/App.js
+++ code/src/App.js
@@ -1,41 +1,13 @@
 import React, { Component } from "react";
 import Blogs from "./components/Blogs";
 import AddBlog from "./components/AddBlog";
+import Navbar from "./components/Navbar";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <a className="navbar-brand" href="#">
-            Blog App
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item active">
-                <a className="nav-link" href="#">
-                  Add a new Blog
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
+        <Navbar />
 
         <div className="container my-5">
           <AddBlog />
