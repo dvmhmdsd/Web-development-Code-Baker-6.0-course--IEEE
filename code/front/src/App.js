9ef268e174ae83e4efb9185ec7bfbae0f72fdd1d
mohamedsaad <mahameds19999@gmail.com>
2019-09-24 00:36:17 +0200

    add sesion code

--- code/front/src/App.js
+++ code/front/src/App.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
-import Navbar from './components/Navbar';
-import BlogList from './components/BlogList';
-import AddBlog from './components/AddBlog';
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import BlogList from "./components/BlogList";
+import AddBlog from "./components/AddBlog";
 
 export default class App extends Component {
   componentDidMount() {
-    console.log("component mounted")
+    console.log("component mounted");
   }
   render() {
     return (
-      <div className="container">
+      <div>
         <Navbar />
-        <BlogList />
-        <AddBlog />
+        <div className="container">
+          <BlogList />
+          <AddBlog />
+        </div>
       </div>
-    )
+    );
   }
 }
