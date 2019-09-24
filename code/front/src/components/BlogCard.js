914aed38c21825755ceef47c6b7281aa5be33664
mohamedsaad <mahameds19999@gmail.com>
2019-09-24 23:06:25 +0200

    add session 10 task

--- code/front/src/components/BlogCard.js
+++ code/front/src/components/BlogCard.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class BlogCard extends Component {
   render() {
     return (
       <div>
         <div className="card">
-                    <div className="card-header">
-                        Featured
-                    </div>
-                    <div className="card-body">
-                        <h5 className="card-title"> { this.props.data.title } </h5>
-                        <p className="card-text">{ this.props.data.body }</p>
-                        <a href="#" className="btn btn-primary">See more</a>
-                    </div>
-                    </div>
+          <div className="card-header">Featured</div>
+          <div className="card-body">
+            <h5 className="card-title"> {this.props.data.title} </h5>
+            <p className="card-text">{this.props.data.body}</p>
+            <a href="#" className="btn btn-primary">
+              See more
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
--- task/README.md
+++ task/README.md
@@ -1,5 +1,5 @@
-# Session 8 Task:-
-It's time to practice what you've learned about `Webpack`.
+# Session 10 Task:-
 
-1. Bundle your `js` files.
-2. Make a production version of your `TODO` App (`puplic` folder) contains `.html`, `.css` & `.js` files bundled & optimized (minimized). 
+Build a `meetup` application that user can: 
+1. View a list of meetups objects. 
+2. Add meetup and preview it while typing and `log` it into the console on submit.
